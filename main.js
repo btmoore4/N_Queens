4de2ef1e0b54f3a48f80e3111b6ed4aad3874858
btmoore4 <btmoore95@gmail.com>
2015-11-11 23:00:59 -0600

    Added logic for multiple solutions

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-var world = 1; 
-function myFunction() {
-		//undraw();
+var world = 0; 
+var num = 0; 
+var solnum = 0; 
+var sol = []; 
+
+function run() {
+		undraw();
+		if ((num == 0) && (world == 0)){
 		var Q = [0,0,0,0,0,0,0,0,0]; 
-		//var Q = [1,2,3,4,3,2,1,2]; 
-		console.log("Hi"); 
-		console.log(world); 
+		console.log("Initialized"); 
 		Queens(Q,1,8);
-		console.log("done"); 
-}
+		console.log("Done"); 
+		world = 1; 
+		}
+		draw(sol[num], 8); 
+		console.log(num); 
+		console.log(solnum); 
+		num++;
+		document.getElementById("soltext").innerHTML = "Solution " + num;
+		if (num >= solnum)
+				num = 0; 
+};
 
 function Queens(Q,r,n) {
 		if (r == n + 1){
-				draw(Q,n); 
-				world++; 
-				throw new Error("Something went badly wrong!");
+				//draw(Q,n);
+				//throw new Error("Something went badly wrong!");
+				var P = Q.slice(); 
+				sol.push(P); 
+				solnum++;
 		}else{
 				for (var j=1; j < (n+1); j++){
 						var legal = true; 	
@@ -35,20 +49,24 @@ function Queens(Q,r,n) {
 		return;
 };
 
-function undraw() {
-		for (var i=1; i<9;i++){
-				for (var j=1; j<9; j++){
-						var coord = ""; 
-						coord = coord.concat("t");  
-						coord = coord.concat(i);  
-						coord = coord.concat(".");  
-						coord = coord.concat(j);  
-						console.log(coord); 
-						var div = document.getElementById(coord);
-						div.style.backgroundColor = 'blue';
-				}
+function clear_all() {
+		num = 0;
+		undraw();
+		document.getElementById("soltext").innerHTML = "Click for Solutions";
+		return; 
+};
 
+function undraw() {
+
+		var col1 = document.getElementsByClassName("col1");
+		var col2 = document.getElementsByClassName("col2");
+		for (var i = 0; i < col1.length; i++){
+			col1[i].style.backgroundColor = 'orange';
 		}
+		for (var i = 0; i < col2.length; i++){
+			col2[i].style.backgroundColor = 'blue';
+		}
+		return;
 };
 
 
@@ -57,9 +75,11 @@ function draw(Q, n) {
 		for (i=0; i < (n); i++){
 				var coord = ""; 
 				coord = coord.concat("t");  
-				coord = coord.concat(i+1);  
-				coord = coord.concat(".");  
 				coord = coord.concat(Q[i+1]);  
+				//coord = coord.concat(i+1);  
+				coord = coord.concat(".");  
+				//coord = coord.concat(Q[i+1]);  
+				coord = coord.concat(i+1);  
 				console.log(coord); 
 				var div = document.getElementById(coord);
 				div.style.backgroundColor = 'red';
